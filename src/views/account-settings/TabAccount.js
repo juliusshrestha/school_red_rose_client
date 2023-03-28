604bb73375b6aaaa1bcee30f62e01b341c8cc091
Julius <juliusshrestha29@gmail.com>
2023-03-28 14:23:37 +0545

    minor fix

--- src/views/account-settings/TabAccount.js
+++ src/views/account-settings/TabAccount.js
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
 import Alert from '@mui/material/Alert'
 import Select from '@mui/material/Select'
-import { styled } from '@mui/material/styles'
+import { styled, useTheme } from '@mui/material/styles'
 import MenuItem from '@mui/material/MenuItem'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
@@ -17,9 +17,11 @@ import IconButton from '@mui/material/IconButton'
 import CardContent from '@mui/material/CardContent'
 import FormControl from '@mui/material/FormControl'
 import Button from '@mui/material/Button'
+import axiosInstance from '../../axios/axiosInstance'
 
 // ** Icons Imports
 import Close from 'mdi-material-ui/Close'
+import { useRouter } from 'next/router'
 
 const ImgStyled = styled('img')(({ theme }) => ({
   width: 120,
@@ -47,43 +49,133 @@ const ResetButtonStyled = styled(Button)(({ theme }) => ({
 
 const TabAccount = () => {
   // ** State
+  const [values, setValues] = useState({})
+  const [firstName, setfirstName] = useState('')
+  const [middleName, setmiddleName] = useState('')
+  const [lastName, setlastName] = useState('')
+  const [userStatus, setuserStatus] = useState('ACTIVE')
+  const [countryCode, setcountryCode] = useState('')
+  const [phone, setphone] = useState('')
+  const [reset, setreset] = useState('')
+  const [saveChanges, setsaveChanges] = useState('')
+
+  // ** Hook
+  const theme = useTheme()
+  const router = useRouter()
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    console.log(firstName)
+
+    const userId = localStorage.getItem('userId')
+    try {
+      const response = await axiosInstance.patch(`user/${userId}/`, {
+        firstName,
+        middleName,
+        lastName,
+        userStatus,
+        countryCode,
+        phone
+      })
+
+      // handle successful response
+      console.log(response.data)
+
+      router.push('/account-settings/', null, { shallow: true })
+    } catch (error) {
+      // handle error response
+      console.error(error)
+    }
+  }
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleReset = prop => event => {
+    {
+      event.preventDefault()
+      event.target.reset()
+    }
+  }
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault()
+  }
 
   return (
     <CardContent>
-      <form>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
         <Grid container spacing={7}>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='firstName' placeholder='John' defaultValue='John' />
+            <TextField
+              fullWidth
+              label='First Name'
+              name='firstName'
+              onChange={e => setfirstName(e.target.value)}
+              placeholder='John'
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='middleName' placeholder='William' defaultValue='William' />
+            <TextField
+              fullWidth
+              label='Middle Name'
+              name='middleName'
+              onChange={e => setmiddleName(e.target.value)}
+              placeholder='William'
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='lastName' placeholder='Doe' defaultValue='Doe' />
+            <TextField
+              fullWidth
+              label='Last Name'
+              name='lastName'
+              onChange={e => setlastName(e.target.value)}
+              placeholder='Doe'
+            />
           </Grid>
 
           <Grid item xs={12} sm={6}>
             <FormControl fullWidth>
               <InputLabel>Status</InputLabel>
-              <Select label='userStatus' defaultValue='active'>
-                <MenuItem value='active'>Active</MenuItem>
-                <MenuItem value='inactive'>Inactive</MenuItem>
-                <MenuItem value='pending'>Closed</MenuItem>
+              <Select
+                label='User Status'
+                name='userStatus'
+                defaultValue='ACTIVE'
+                onChange={e => setuserStatus(e.target.value)}
+              >
+                <MenuItem value='ACTIVE'>Active</MenuItem>
+                <MenuItem value='INACTIVE'>Inactive</MenuItem>
+                <MenuItem value='PENDING'>Closed</MenuItem>
               </Select>
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth type='number' label='countryCode' placeholder='977' />
+            <TextField
+              fullWidth
+              type='number'
+              onChange={e => setcountryCode(e.target.value)}
+              label='Country Code'
+              name='countryCode'
+              placeholder='977'
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth type='text' label='phone' placeholder='(123) 456-7890' />
+            <TextField
+              fullWidth
+              type='text'
+              onChange={e => setphone(e.target.value)}
+              label='Phone'
+              name='phone'
+              placeholder='(123) 456-7890'
+            />
           </Grid>
 
           <Grid item xs={12}>
-            <Button variant='contained' sx={{ marginRight: 3.5 }}>
+            <Button variant='contained' type='submit' sx={{ marginRight: 3.5 }}>
               Save Changes
             </Button>
-            <Button type='reset' variant='outlined' color='secondary'>
+            <Button type='reset' variant='outlined' onClick={handleReset()} color='secondary'>
               Reset
             </Button>
           </Grid>
