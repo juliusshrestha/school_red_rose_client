a51704c6c3eb6297fe9493026fe2e63736c54211
Julius <juliusshrestha29@gmail.com>
2023-03-29 13:56:00 +0545

    interceptor added

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,52 +1,30 @@
 import axios from 'axios'
-
-let token = ''
-if (typeof window !== 'undefined') {
-  // Perform localStorage action
-  token = String(localStorage.getItem('token'))
-}
-
-var value = 'en'
+import router, { useRouter } from 'next/router'
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/',
   headers: {
-    Authorization: `Bearer ${token}`,
-    'Access-Controll-Allow-Origin': '',
-    accept: 'application/json'
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
   }
-
-  // baseURL: 'https://www.asha-holdings.com/api/'
 })
 
-// let axiosInstance
+axiosInstance.interceptors.response.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers['Authorization'] = token
+    }
 
-// if (value === 'en') {
-//   axiosInstance = axios.create({
-//     baseURL: 'http://localhost:3000/',
-//     headers: {
-//       Authorization: process.env.REACT_APP_API_KEY
-//     }
-//   })
-// } else if (value === 'ja') {
-//   axiosInstance = axios.create({
-//     baseURL: 'http://localhost:3000/',
-//     headers: {
-//       Authorization: process.env.REACT_APP_API_KEY
-//     }
-//   })
-// } else {
-//   axiosInstance = axios.create({
-//     baseURL: 'http://localhost:3000/',
-//     headers: {
-//       Authorization: process.env.REACT_APP_API_KEY
-//     }
-//   })
-// }
+    return config
+  },
+  error => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token')
+    }
 
-// const axiosInstance = axios.create();
-// axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
-// axiosInstance.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-// axiosInstance.defaults.xsrfCookieName = "csrftoken";
-// axiosInstance.defaults.withCredentials = true;
+    return Promise.reject(error)
+  }
+)
+
 export default axiosInstance
