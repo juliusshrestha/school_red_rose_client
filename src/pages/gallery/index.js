3beed718aeef5db1b6654068e6aafd148bfe9161
Julius <juliusshrestha29@gmail.com>
2023-04-05 22:18:31 +0545

    album tab added

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -16,7 +16,7 @@ import LockOpenOutline from 'mdi-material-ui/LockOpenOutline'
 import InformationOutline from 'mdi-material-ui/InformationOutline'
 
 // ** Demo Tabs Imports
-
+import TabAlbum from 'src/views/image-upload/TabAlbum'
 import TabGallery from 'src/views/image-upload/TabGallery'
 
 // ** Third Party Styles Imports
@@ -42,7 +42,7 @@ const TabName = styled('span')(({ theme }) => ({
 
 const Gallery = () => {
   // ** State
-  const [value, setValue] = useState('account')
+  const [value, setValue] = useState('album')
   const message = ''
 
   const handleChange = (event, newValue) => {
@@ -57,8 +57,18 @@ const Gallery = () => {
           aria-label='post-upload tabs'
           sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}
         >
+          {' '}
           <Tab
-            value='account'
+            value='album'
+            label={
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Post />
+                <TabName>Creat your ALbums</TabName>
+              </Box>
+            }
+          />
+          <Tab
+            value='photo'
             label={
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <Post />
@@ -67,8 +77,10 @@ const Gallery = () => {
             }
           />
         </TabList>
-
-        <TabPanel sx={{ p: 0 }} value='account'>
+        <TabPanel sx={{ p: 0 }} value='album'>
+          <TabAlbum />
+        </TabPanel>
+        <TabPanel sx={{ p: 0 }} value='photo'>
           <TabGallery />
         </TabPanel>
       </TabContext>
